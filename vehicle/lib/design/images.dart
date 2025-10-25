e17b4a5945dab69dd3e604edc1d839d227fe2318
DovganMariya <feya.forests.05@mail.ru>
2025-10-25 19:59:34 +0300

    added all images

--- vehicle/lib/design/images.dart
+++ vehicle/lib/design/images.dart
@@ -2,3 +2,36 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 final SvgPicture vehicleMotorcycleImage =
     SvgPicture.asset('assets/images/vehicle_motorcycle.svg');
+
+final SvgPicture stateBoardingImage =
+    SvgPicture.asset('assets/images/state_boarding.svg');
+
+final SvgPicture stateDeliveryImage =
+    SvgPicture.asset('assets/images/state_delivery.svg');
+
+final SvgPicture stateLoadImage =
+    SvgPicture.asset('assets/images/state_load.svg');
+
+final SvgPicture stateParkingImage =
+    SvgPicture.asset('assets/images/state_parking.svg');
+
+final SvgPicture statePickupImage =
+    SvgPicture.asset('assets/images/state_pickup.svg');
+
+final SvgPicture stateRepairImage =
+    SvgPicture.asset('assets/images/state_repair.svg');
+
+final SvgPicture stateUnboardingImage =
+    SvgPicture.asset('assets/images/state_unboarding.svg');
+
+final SvgPicture stateUnloadImage =
+    SvgPicture.asset('assets/images/state_unload.svg');
+
+final SvgPicture vehicleBusImage =
+    SvgPicture.asset('assets/images/vehicle_bus.svg');
+
+final SvgPicture vehicleTruckImage =
+    SvgPicture.asset('assets/images/vehicle_truck.svg');
+
+final SvgPicture vehicleTuktukImage =
+    SvgPicture.asset('assets/images/vehicle_tuktuk.svg');
