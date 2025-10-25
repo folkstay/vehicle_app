8b7d8313bca675039bf61e5752ad7c658096df05
DovganMariya <feya.forests.05@mail.ru>
2025-10-25 18:35:01 +0300

    svg
    -added svg

---
+++ vehicle/lib/design/images.dart
@@ -0,0 +1,4 @@
+import 'package:flutter_svg/flutter_svg.dart';
+
+final SvgPicture vehicleMotorcycleImage =
+    SvgPicture.asset('assets/images/vehicle_motorcycle.svg');
--- vehicle/lib/home_page.dart
+++ vehicle/lib/home_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:vehicle/design/images.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text(
-          'Flutter by kiparo.com',
-        ),
+        child: vehicleMotorcycleImage,
       ),
     );
   }
