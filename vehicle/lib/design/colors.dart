8a2a0d3e3105caaefc6db2dac61f1cc197c5823d
DovganMariya <feya.forests.05@mail.ru>
2025-10-25 18:21:36 +0300

    added colors
    added colors

---
+++ vehicle/lib/design/colors.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+const Color primaryColor = Color(0xFFFF8E1F);
+
+const Color secondaryColor = Color(0xFF081B31);
+
+const Color secondaryVariantColor = Color(0x80081B31);
+
+const Color backgroundColor = Color(0xFFF2F2F2);
+
+const Color surfaceColor = Color(0xFFFFFFFF);
