861a4f3818c7232ead11609c6c9c8c316c5ed023
DovganMariya <feya.forests.05@mail.ru>
2025-10-26 17:23:13 +0300

    added AppBar
    -added AppBar

--- vehicle/lib/pages/main.dart
+++ vehicle/lib/pages/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:vehicle/home_page.dart';
+import 'package:vehicle/pages/vehicle/vehicle_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const HomePage(),
+      home: const VehiclePage(),
     );
   }
 }
---
+++ vehicle/lib/pages/vehicle/vehicle_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:vehicle/design/colors.dart';
+
+class VehiclePage extends StatelessWidget {
+  const VehiclePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: surfaceColor,
+        centerTitle: true,
+        title: const Text('Kiparo.com Dispatcher',
+            style: TextStyle(
+                color: primaryColor,
+                fontSize: 16,
+                fontWeight: FontWeight.w500)),
+      ),
+      body: Container(color: backgroundColor, child: const Text('Body')),
+    );
+  }
+}
