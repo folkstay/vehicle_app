28a679977d0ea9439d071d1d1beb0f30dab58802
DovganMariya <feya.forests.05@mail.ru>
2025-11-07 15:07:56 +0300

    Text
    added Text

--- vehicle/lib/pages/vehicle/vehicle_item.dart
+++ vehicle/lib/pages/vehicle/vehicle_item.dart
@@ -7,25 +7,50 @@ class VehicleItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: surfaceColor,
-      elevation: 0.06,
-      margin: EdgeInsets.zero,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
-      child: InkWell(
-        borderRadius: BorderRadius.circular(8),
-        onTap: () {},
-        child: Padding(
-          padding: const EdgeInsets.only(left: 0, right: 16),
-          child: Row(
-              children: <Widget>[vehicleMotorcycleImage, _title(), _state()]),
+    return SizedBox(
+      height: 64,
+      child: Card(
+        color: surfaceColor,
+        elevation: 0.06,
+        margin: EdgeInsets.zero,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+        child: InkWell(
+          borderRadius: BorderRadius.circular(8),
+          onTap: () {},
+          child: Padding(
+            padding: const EdgeInsets.only(left: 0, right: 16),
+            child: Row(
+                children: <Widget>[vehicleMotorcycleImage, _title(), _state()]),
+          ),
         ),
       ),
     );
   }
 
   Widget _title() {
-    return Container();
+    return const Padding(
+      padding: EdgeInsets.only(left: 6, right: 6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            'BMW GS-7638',
+            style: TextStyle(
+                color: secondaryColor,
+                fontSize: 14,
+                fontWeight: FontWeight.w600),
+          ),
+          Text(
+            'No driver',
+            style: TextStyle(
+                color: secondaryVariantColor,
+                fontSize: 14,
+                fontWeight: FontWeight.w400),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _state() {
